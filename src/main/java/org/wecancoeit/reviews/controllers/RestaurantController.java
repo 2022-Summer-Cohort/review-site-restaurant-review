f80aac83745f825cc2dfed8f1b17aadacba12c5f
Patrick Hastings <phastings82@gmail.com>
2022-06-29 10:19:50 -0400

    build fixed: app launch successful, localhost8080 server successful. Please clone this version

--- src/main/java/org/wecancoeit/reviews/controllers/RestaurantController.java
+++ src/main/java/org/wecancoeit/reviews/controllers/RestaurantController.java
@@ -6,51 +6,51 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.wecancoeit.reviews.model.Hashtag;
 import org.wecancoeit.reviews.model.Restaurant;
-import org.wecancoeit.reviews.repos.HashtagRepository;
+import org.wecancoeit.reviews.model.Review;
 import org.wecancoeit.reviews.repos.RestaurantRepository;
+import org.wecancoeit.reviews.repos.ReviewRepository;
 
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/restaurants")
 public class RestaurantController {
-    private RestaurantRepository restaurantRepo;
-    private HashtagRepository hashtagRepo;
 
-    public RestaurantController(RestaurantRepository restaurantRepo, HashtagRepository hashtagRepo) {
+    private ReviewRepository reviewRepo;
+    private RestaurantRepository restaurantRepo;
+
+    public RestaurantController(ReviewRepository reviewRepo, RestaurantRepository restaurantRepo) {
+        this.reviewRepo = reviewRepo;
         this.restaurantRepo = restaurantRepo;
-        this.hashtagRepo = hashtagRepo;
     }
 
     @RequestMapping("/{id}")
-    public String showRestaurant(Model model, @PathVariable Long id) {
-        model.addAttribute("whatEverIwant", restaurantRepo.findById(id).get());
+    public String showRestaurant(Model model, @PathVariable Long id){
+        model.addAttribute("LosGuachos", restaurantRepo.findById(id).get());
         return "restaurant";
     }
 
     @RequestMapping("/")
-    public String showAllRestaurants(Model model) {
+    public String showAllRestaurants(Model model){
         model.addAttribute("restaurants", restaurantRepo.findAll());
         return "allRestaurants";
     }
-
-    @PostMapping("/{id}/addHashtag")
-    public String addHashtagToRestaurant(@PathVariable Long id, @RequestParam String hashtag) {
+    @PostMapping("/{id}/addReview")
+    public String addReviewToRestaurant(@PathVariable Long id, @RequestParam String review) {
         Restaurant restaurant = restaurantRepo.findById(id).get();
-        Optional<Hashtag> hashtagOptional = hashtagRepo.findByNameIgnoreCase(hashtag);
-        if (hashtagOptional.isPresent()) {
-            if (!restaurant.getHashtags().contains(hashtagOptional.get())) {
-                restaurant.addHashtag(hashtagOptional.get());
+        Optional<Review> reviewOptional = reviewRepo.findByRatingIgnoreCase(review);
+        if (!reviewOptional.isPresent()) {
+            if (!restaurant.getReviews().contains(reviewOptional.get())) {
+                 restaurant.addReview(reviewOptional.get());
             }
-        } else {
-            Hashtag hashtag1 = new Hashtag(hashtag);
-            hashtagRepo.save(hashtag1);
-            restaurant.addHashtag(hashtag1);
+        }
+        else {
+            Review review1 = new Review(review);
+            reviewRepo.save(review1);
+            restaurant.addReview(review1);
         }
         restaurantRepo.save(restaurant);
-
-        return "redirect:/restaurant/"+id;
+        return "redirect:/restaurants/"+id;
     }
 }
